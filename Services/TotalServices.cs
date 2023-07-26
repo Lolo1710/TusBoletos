b189c59cd1a42da6d8ca0d4d3d071906768c1c9d
LuzAcosta12 <luzbetsabe.12@gmail.com>
2023-07-25 19:37:38 -0500

    boton

---
+++ Services/TotalServices.cs
@@ -0,0 +1,44 @@
+﻿using ProyectotTUSBOLETOS.Context;
+using ProyectotTUSBOLETOS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectotTUSBOLETOS.Services
+{
+    public class TotalServices
+    {
+        public decimal Monto()
+        {
+            using (var _context = new ApplicationDbContext())
+            {
+                decimal total;
+                total = _context.Ventas.Sum(x => x.Total);
+                return total;
+            }
+        }
+
+        public List<Venta> GetVentas()
+        {
+            try
+            {
+                using (var _context = new ApplicationDbContext())
+                {
+
+                    List<Venta> ventas = new List<Venta>();
+
+                    return _context.Ventas.ToList();
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Succedio un error " + ex.Message);
+            }
+
+        }
+    }
+}
--- Vistas/Ventas.xaml.cs
+++ Vistas/Ventas.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using LiveCharts;
 using LiveCharts.Wpf;
+using ProyectotTUSBOLETOS.Services;
 
 namespace ProyectotTUSBOLETOS.Vistas
 {
@@ -21,9 +22,26 @@ namespace ProyectotTUSBOLETOS.Vistas
     /// </summary>
     public partial class Ventas : Window
     {
+        TotalServices services = new TotalServices();
         public Ventas()
         {
             InitializeComponent();
+            GetUserTable();
+            Gtotal();
+        }
+
+        decimal gtotal;
+        TotalServices total = new TotalServices();
+
+        public void GetUserTable()
+        {
+            Eventos.ItemsSource = services.GetVentas();
+        }
+
+        public void Gtotal()
+        {
+            gtotal = total.Monto();
+            lblTotal.Content = gtotal;
         }
 
         private void back_Click(object sender, RoutedEventArgs e)
