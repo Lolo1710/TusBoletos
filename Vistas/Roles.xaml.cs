93ad3fedb6ebc8f5e9a048cb5442037227269027
LuzAcosta12 <luzbetsabe.12@gmail.com>
2023-07-27 21:39:44 -0500

    roles

---
+++ Vistas/Roles.xaml.cs
@@ -0,0 +1,98 @@
+﻿using LiveCharts.Wpf;
+using ProyectotTUSBOLETOS.Entities;
+using ProyectotTUSBOLETOS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectotTUSBOLETOS.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para Roles.xaml
+    /// </summary>
+    public partial class Roles : Window
+    {
+        //RolesServices services = new RolesServices();
+        public Roles()
+        {
+            InitializeComponent();
+            GetRolesTable();
+        }
+
+        public void GetRolesTable()
+        {
+            //UserTable.ItemsSource = services.GetRoles();
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Rol roles = new Rol();
+
+            if (txtPkUser.Text == "")
+            {
+                roles.Nombre = txtNombre.Text;
+
+                //services.AddRoles(roles);
+
+                txtNombre.Clear();
+
+                MessageBox.Show("Se agrego correctamente");
+                GetRolesTable();
+            }
+
+            else
+            {
+                roles.PkRol = int.Parse(txtPkUser.Text);
+                roles.Nombre = txtNombre.Text;
+
+                //services.UpdateRol(roles);
+
+                txtPkUser.Clear();
+                txtNombre.Clear();
+
+                MessageBox.Show("Se actualizo correctamente");
+                GetRolesTable();
+            }
+        }
+
+        public void EditItem(object sender, RoutedEventArgs e)
+        {
+            Rol rol = new Rol();
+
+            rol = (sender as FrameworkElement).DataContext as Rol; //Esta funcion traera la selecion de la fila 
+
+            txtPkUser.Text = rol.PkRol.ToString();
+            txtNombre.Text = rol.Nombre.ToString();
+        }
+
+        private void ElimiarItem(object sender, RoutedEventArgs e)
+        {
+            Rol rol = new Rol();
+            Roles services = new Roles();
+            rol = (sender as FrameworkElement).DataContext as Rol;
+            rol.PkRol.ToString();
+            //services.DeleteRol(rol);
+
+            MessageBox.Show("Se elimino el usuario");
+            GetRolesTable();
+        }
+
+        private void btnLimpiar_Click(object sender, RoutedEventArgs e)
+        {
+            txtPkUser.Clear();
+            txtNombre.Clear();
+        }
+
+    }
+}
---
+++ Vistas/SuperAdmin.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectotTUSBOLETOS.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para SuperAdmin.xaml
+    /// </summary>
+    public partial class SuperAdmin : Window
+    {
+        public SuperAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddUSer_Click(object sender, RoutedEventArgs e)
+        {
+            Sistema sistema = new Sistema();
+            Close();
+            sistema.Show();
+        }
+
+        private void btnAddEventos_Click(object sender, RoutedEventArgs e)
+        {
+            Eventos evento = new Eventos();
+            Close();
+            evento.Show();
+        }
+
+        private void btnVentas_Click(object sender, RoutedEventArgs e)
+        {
+            Ventas venta = new Ventas();
+            Close();
+            venta.Show();
+        }
+
+        private void btnAddRoles_Click(object sender, RoutedEventArgs e)
+        {
+            Roles roles = new Roles();
+            Close();
+            roles.Show();
+        }
+    }
+}
+
